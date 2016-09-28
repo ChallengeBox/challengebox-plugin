ec4e201210ac5057f486d2d0e8830c3799aee303
Bret Sheeley <carteeg@gmail.com>
2016-09-28 15:49:47 -0400

    Fix sql.

--- database_updates/2016-09-13-1500.sql
+++ database_updates/2016-09-13-1500.sql
@@ -1,14 +1,11 @@
-CREATE TABLE `wp_raw_tracking_data` 
-( 
-	`user_id` BIGINT NOT NULL , 
-	`date` DATE NOT NULL , 
-	`source` CHAR(64) NOT NULL , 
-	`data` TEXT NOT NULL , 
-	`create_date` TIMESTAMP NOT NULL , 
-	`last_modified` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL , 
-	UNIQUE KEY (`user_id`, `date`, `source`)
-) 
-ENGINE = InnoDB;
+CREATE TABLE `wp_raw_tracking_data` (
+  `user_id` bigint(20) NOT NULL,
+  `date` date NOT NULL,
+  `source` char(64) NOT NULL,
+  `data` text NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `raw_tracking_data` 
+ALTER TABLE `wp_raw_tracking_data` 
 ADD INDEX (`user_id`);
