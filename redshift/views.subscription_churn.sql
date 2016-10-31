ef40f7255ebd5d6cb0e8d900d6ad25b72cc29e1c
Ryan Witt <onecreativenerd@gmail.com>
2016-10-31 15:01:48 -0400

    fix typo

--- redshift/views.subscription_churn.sql
+++ redshift/views.subscription_churn.sql
@@ -156,7 +156,7 @@ CREATE VIEW subscription_churn_stage0 AS
         subscription_churn_month_ends
     ORDER BY
         user_id, subscription_id, event_date, id DESC
-)
+;
 
 DROP VIEW IF EXISTS subscription_churn_stage1 CASCADE;
 CREATE VIEW subscription_churn_stage1 AS
@@ -211,43 +211,43 @@ CREATE VIEW subscription_churn_by_calendar_month AS
 
 DROP VIEW IF EXISTS subscription_churn_by_calendar_month_debug CASCADE;
 CREATE VIEW subscription_churn_by_calendar_month_debug AS
-(
-    SELECT * FROM subscription_churn_by_calendar_month
-)
-UNION
-(
-    SELECT 
-          '2016-10 (-total-)' AS calendar_month
-        , max(number_of_users) AS number_of_users
-        , max(number_of_subs) AS number_of_subs
-        , sum(activated) AS activated
-        , NULL AS active
-        , sum(churned) AS churned
-        , round(100.0 * sum(churned) / max(number_of_users))::INTEGER AS churn_pct
-        , sum(reactivated) AS reactivated
-    FROM
-        subscription_churn_by_calendar_month
-    GROUP BY
+    (
+        SELECT * FROM subscription_churn_by_calendar_month
+    )
+    UNION
+    (
+        SELECT 
+              '2016-10 (-total-)' AS calendar_month
+            , max(number_of_users) AS number_of_users
+            , max(number_of_subs) AS number_of_subs
+            , sum(activated) AS activated
+            , NULL AS active
+            , sum(churned) AS churned
+            , round(100.0 * sum(churned) / max(number_of_users))::INTEGER AS churn_pct
+            , sum(reactivated) AS reactivated
+        FROM
+            subscription_churn_by_calendar_month
+        GROUP BY
+            1
+    )
+    UNION
+    (
+        SELECT
+              to_char(sysdate, 'YYYY-MM (from subs table)') AS calendar_month
+            , count(user_id) AS number_of_users
+            , count(id) AS number_of_subs
+            , NULL AS activated
+            , count(CASE WHEN status = 'active' THEN 1 end) AS active
+            , count(CASE WHEN status <> 'active' THEN 1 end) AS churned
+            , round(100.0 * count(CASE WHEN status <> 'active' THEN 1 end) / count(user_id))::INTEGER AS churn_pct
+            , NULL AS reactivated
+        FROM
+            subscriptions
+        GROUP BY
         1
-)
-UNION
-(
-    SELECT
-          to_char(sysdate, 'YYYY-MM (from subs table)') AS calendar_month
-        , count(user_id) AS number_of_users
-        , count(id) AS number_of_subs
-        , NULL AS activated
-        , count(CASE WHEN status = 'active' THEN 1 end) AS active
-        , count(CASE WHEN status <> 'active' THEN 1 end) AS churned
-        , round(100.0 * count(CASE WHEN status <> 'active' THEN 1 end) / count(user_id))::INTEGER AS churn_pct
-        , NULL AS reactivated
-    FROM
-        subscriptions
-    GROUP BY
-    1
-)
-ORDER BY
-    calendar_month
+    )
+    ORDER BY
+        calendar_month
 ;
 
 COMMIT;
