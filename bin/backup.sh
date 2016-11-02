c3dd9294bbe5b84ea5dac69231655f0123bba811
Ryan Witt <onecreativenerd@gmail.com>
2016-11-02 09:35:59 -0400

    exclude cache directory from backup

--- bin/backup.sh
+++ bin/backup.sh
@@ -10,9 +10,9 @@ TIMESTAMP=`date -u +%Y-%m-%dT%H:%M:%SZ`
 echo "Cleaning old files:"
 find ${BDIR}/* -mtime +7 -exec rm {} \; || echo "No old files found"
 echo "Backing up wordpress directory:"
-tar -czf ${BDIR}/challengebox_${TIMESTAMP}.tgz /var/www/box/
+tar -czf --exclude='/var/www/box/wp-content/cache' ${BDIR}/challengebox_${TIMESTAMP}.tgz /var/www/box/
 echo "Backing up wordpress directory with exclusions:"
-tar --exclude='/var/www/box/wp-config.php' -czf ${BDIR}/clean_challengebox_${TIMESTAMP}.tgz /var/www/box/
+tar --exclude='/var/www/box/wp-config.php' --exclude='/var/www/box/wp-content/plugins/challengebox/vendor' --exclude='/var/www/box/wp-content/cache' -czf ${BDIR}/clean_challengebox_${TIMESTAMP}.tgz /var/www/box/
 echo "Backing up database:"
 mysqldump fit_box | gzip > ${BDIR}/challengebox_${TIMESTAMP}.sql.gz
 echo "Backing up database with exclusions:"
