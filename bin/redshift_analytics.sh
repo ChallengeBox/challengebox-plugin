20fde1a500175a6dcdf5db24588ac81b51eac74a
Ryan Witt <onecreativenerd@gmail.com>
2016-11-30 09:13:29 -0500

    spin up and spin down redshift cluster as needed

--- bin/redshift_analytics.sh
+++ bin/redshift_analytics.sh
@@ -2,6 +2,17 @@
 set -eo pipefail
 [[ "$TRACE" ]] && set -x
 
+# Start cluster
+aws redshift restore-from-cluster-snapshot --cluster-identifier cb-analytics --snapshot-identifier cb-analytics-nightly
+
+# Wait for cluster to spin up
+CLUSTER_WAIT=60
+while aws redshift describe-clusters --cluster-identifier cb-analytics | grep ClusterStatus | grep -v available
+do
+	echo "Waiting $CLUSTER_WAIT seconds to check cluster status..."
+	sleep $CLUSTER_WAIT
+done
+
 # Export to s3
 sudo -u www-data wp cb export_users --all --redshift --redshift-upload-only "$@"
 sudo -u www-data wp cb export_subscriptions --all --redshift --redshift-upload-only "$@"
@@ -14,3 +25,14 @@ sudo -u www-data wp cb export_fitness_data --redshift --redshift-upload-only "$@
 # Load data into db
 sudo -u www-data wp cb reload_redshift "$@"
 
+# Shutdown cluster
+aws redshift delete-cluster-snapshot --snapshot-identifier cb-analytics-nightly
+aws redshift delete-cluster --cluster-identifier cb-analytics --final-cluster-snapshot-identifier cb-analytics-nightly
+
+# Wait for cluster to spin up
+CLUSTER_WAIT=60
+while aws redshift describe-clusters --cluster-identifier cb-analytics
+do
+	echo "Waiting $CLUSTER_WAIT seconds for cluster shutdown..."
+	sleep $CLUSTER_WAIT
+done
